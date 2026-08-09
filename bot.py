08564e4ee650ef8579be6291dd5ea102b512ac2a
malkoggu-beep <malkoggu@gmail.com>
2026-08-10 02:57:45 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,3 @@
-from flask import Flask
-from threading import Thread
-import discord  # أو أي مكتبات ثانية عندك
-
-app = Flask('')
-
-
-@app.route('/')
-def home():
-  return "I'm alive!"
-
-
-def run():
-  app.run(host='0.0.0.0', port=8080)
-
-
-def keep_alive():
-  t = Thread(target=run)
-  t.start()
-
-
-# تشغيل السيرفر الوهمي أولاً
-keep_alive()
-
-# هنا يبدأ كود بوت ديسكورد العادي حقك (مثل bot.run أو client.run)
 import discord
 import os
 from groq import Groq
